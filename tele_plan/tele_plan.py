18a69a4db6f9a03774c691075d4b6e003d76d2ca
Krystian Postek <krystian@postek.eu>
2022-03-27 21:10:02 +0200

    Overall rework of main codebase

--- tele_plan/tele_plan.py
+++ tele_plan/tele_plan.py
@@ -1,55 +1,51 @@
-
 import asyncio
-from html import entities
 import logging
 import os
+from tkinter import S
 from typing import List
 import httpx
+from urllib.parse import urlencode
 
-from datetime import date
-from aiogram import Bot, Dispatcher, types, utils
-from timetable_entry import Entry, from_json, to_markdown
+from datetime import date, timedelta
+from aiogram import Bot, Dispatcher, types, utils, executor
+from timetable_entry import Entry
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
-    )
+)
 
 logger = logging.getLogger(__name__)
 
+# Init bot and dispatcher
+if not os.getenv("TELEGRAM_BOT_API"):
+    raise Exception("TELEGRAM_BOT_API env variable not set")
 
+bot = Bot(token=os.getenv("TELEGRAM_BOT_API"))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["today"])
 async def get_timetable(telegram_message: types.Message):
     date_today = date.today()
     # TODO: Make groups customizable by env (?) or select them at `/start`
-    base_url="https://altapi.kpostek.dev/v1/timetable/date/{}?groups={}&groups={}".format(date_today,"WIs I.2 - 46c","WIs I.2 - 1w")
+    url = f"https://altapi.kpostek.dev/v1/timetable/date/{date_today.isoformat()}"
+    # url = base_url + urlencode({"groups": ["WIs I.2 - 46c", "WIs I.2 - 1w"]})
+
     async with httpx.AsyncClient() as client:
-        r = await client.get(base_url)
-    json = r.json()
-    if len(json['entries']) == 0:
+        r = await client.get(url, params={"groups": ["WIs I.2 - 46c", "WIs I.2 - 1w"]})
+    payload = r.json()
+    print(url, payload)
+
+    if len(payload['entries']) == 0:
         message = utils.markdown.bold("Dzisiaj nie masz lekcji! 😎")
-        await telegram_message.answer(message,types.ParseMode.MARKDOWN_V2)
+        await telegram_message.answer(message, types.ParseMode.MARKDOWN_V2)
     else:
         message = "*Oto twój plan lekcji na dziś:*\n"
-        entries: List[Entry] = []
-        for entry in json['entries']:
-            entries.append(from_json(entry))
-        await telegram_message.answer(message + "".join(map(to_markdown,entries)),parse_mode=types.ParseMode.MARKDOWN_V2)
+        entries: List[Entry] = list(map(Entry.from_json, payload['entries']))
 
-async def main():
-    # Grab an API token from `env``
-    token = os.getenv("TELEGRAM_BOT_API")
-    # Create the Bot
-    bot = Bot(token)
-    try:
-        # Get the dispatcher to register handlers
-        dispatcher = Dispatcher(bot)
-        # Register commands
-        dispatcher.register_message_handler(get_timetable,commands={"today"})
-        # Start the bot
-        await dispatcher.start_polling()
-    finally:
-        await bot.close()
+        await telegram_message.answer(message + "".join(map(lambda x: x.to_markdown(), entries)), parse_mode=types.ParseMode.MARKDOWN_V2)
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
-    
+    executor.start_polling(dp, skip_updates=True)
