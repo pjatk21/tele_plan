a37633c59c4bdc0542f0839a5233fa0b50d52ce9
Krystian Postek <krystian@postek.eu>
2022-03-27 21:09:32 +0200

    Move functions into class to preserve type safety

--- tele_plan/timetable_entry.py
+++ tele_plan/timetable_entry.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 from datetime import datetime, time
-from typing import List,Dict, Optional
+from typing import List, Dict, Optional
 from aiogram import utils
+
 
 @dataclass
 class Entry:
@@ -14,37 +15,37 @@ class Entry:
     room: str
     type: str
     tutor: Optional[str] = None
-    
 
-def to_markdown(entry: Entry) -> str:
-    if entry.type == "Wykład":
-        markdown = "{} - {} | {}: {} (zdalny wykład na Teams)\n".format(
-            time.isoformat(entry.begin.time(),timespec='minutes'),
-            time.isoformat(entry.end.time(),timespec='minutes'),
-            entry.code,
-            entry.name
-            )
-    else:
-        markdown = "{} - {} | {}: {} (sala {} w budynku {})\n".format(
-            time.isoformat(entry.begin.time(),timespec='minutes'),
-            time.isoformat(entry.end.time(),timespec='minutes'),
-            entry.code,
-            entry.name,
-            entry.room,
-            entry.building
-            )
-    return utils.markdown.escape_md(markdown)
-
-def from_json(json: Dict) -> Entry:
-    value = Entry(
-        begin=datetime.fromisoformat(json['begin'][:-1]),
-        building=json['building'],
-        code = json['code'],
-        end=datetime.fromisoformat(json['end'][:-1]),
-        groups=json['groups'],
-        name=json['name'],
-        room=json['room'],
-        type=json['type'],
-        tutor=json['tutor']
+    @classmethod
+    def from_json(self, json: Dict):
+        return self(
+            begin=datetime.fromisoformat(json['begin'][:-1]),
+            building=json['building'],
+            code=json['code'],
+            end=datetime.fromisoformat(json['end'][:-1]),
+            groups=json['groups'],
+            name=json['name'],
+            room=json['room'],
+            type=json['type'],
+            tutor=json['tutor']
         )
-    return value
+
+
+    def to_markdown(self) -> str:
+        if self.type == "Wykład":
+            markdown = "{} - {} | {}: {} (zdalny wykład na Teams)\n".format(
+                time.isoformat(self.begin.time(), timespec='minutes'),
+                time.isoformat(self.end.time(), timespec='minutes'),
+                self.code,
+                self.name
+            )
+        else:
+            markdown = "{} - {} | {}: {} (sala {} w budynku {})\n".format(
+                time.isoformat(self.begin.time(), timespec='minutes'),
+                time.isoformat(self.end.time(), timespec='minutes'),
+                self.code,
+                self.name,
+                self.room,
+                self.building
+            )
+        return utils.markdown.escape_md(markdown)
